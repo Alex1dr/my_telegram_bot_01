d76a0a191d46809aa8e74bee24418bb9ddb57670
veris <4214951@gmail.com>
2024-03-29 23:53:09 +0300

    add italic

--- project_folder/handlers.py
+++ project_folder/handlers.py
@@ -1,12 +1,12 @@
 from aiogram.filters import CommandStart
 from aiogram import types
 from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from aiogram.utils.markdown import hbold, italic
 from loader import dp
 
 @dp.message(CommandStart())
 async def command_start_handler(message: types.Message) -> None:
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}")
+    await message.answer(f"Hello, {italic(message.from_user.full_name)}")
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
